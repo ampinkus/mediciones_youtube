7e5cc9fc8f49a395568d8b7732f819ff668b4ece
Alfredo Pinkus <ampinkus@gmail.com>
2025-07-14 12:12:12 -0300

    modificado nombre bases de datos por mediciones _edes

--- src/database/database.js
+++ src/database/database.js
@@ -2,7 +2,7 @@
 import { Sequelize } from 'sequelize';
 
 // ✅ Configuración de conexión a PostgreSQL
-const sequelize = new Sequelize('youtube_gervasio', 'postgres', 'Hammil_01', {
+const sequelize = new Sequelize('medicion_redes', 'postgres', 'Hammil_01', {
     host: 'localhost',
     port: 5432,
     dialect: 'postgres',
